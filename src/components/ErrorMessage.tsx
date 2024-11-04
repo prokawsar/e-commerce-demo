23fc96842572511d4435b7c99d244a28a8d75301
prokawsar <prokawsar@gmail.com>
2024-11-04 08:59:15 +0600

    Error message

---
+++ src/components/ErrorMessage.tsx
@@ -0,0 +1,13 @@
+import { Icon } from "@iconify/react";
+
+export default function ErrorMessage({ message }: { message: string }) {
+  return (
+    <div className="max-w-2xl mx-auto mt-8 p-4">
+      <div className="bg-red-50 border border-red-500 rounded-md flex items-center flex-col justify-center p-5">
+        <Icon icon="mdi:multiply" className="text-red-700" width="30px" />
+        <h2 className="text-lg py-2">Error</h2>
+        <p className="text-center">{message}</p>
+      </div>
+    </div>
+  );
+}
--- src/error.tsx
+++ src/error.tsx
@@ -1,10 +1,16 @@
-import { Link } from "react-router-dom";
+import { ErrorResponse, Link, useRouteError } from "react-router-dom";
+import ErrorMessage from "./components/ErrorMessage";
 
 export default function Error() {
+  const error = useRouteError() as ErrorResponse;
+
   return (
     <div className="flex w-full flex-1 flex-col justify-center gap-2 h-screen items-center">
       <div className="items-center  flex w-full flex-1 flex-col justify-center gap-2">
-        <p className="text-red-600">Error ! Not found</p>
+        {error.status == 404 && <ErrorMessage message={error.data} />}
+        {error.status != 404 && (
+          <ErrorMessage message={"Check your network!"} />
+        )}
         <Link to={"/"}>Return Home</Link>
       </div>
     </div>
